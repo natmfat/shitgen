f2587b66b50eb406d0a53818590b0f7f04c12d5e
Nathan <146884027+natmfat@users.noreply.github.com>
2024-10-24 19:42:55 -0700

    fix: types ahhhh

--- src/template/Model.ts
+++ src/template/Model.ts
@@ -23,46 +23,45 @@ type UserData = {
   random: string | null;
 };
 
-type PostData = {
-  id: number;
-  title: string | null;
-  body: string;
-  userId: UserData["id"];
-};
-
 type NonNullValue<NonNull extends true | false, value> = NonNull extends true
   ? value
   : value | null;
 
-type WhereOperatorString<NonNull extends true | false> = {
-  eq: NonNullValue<NonNull, string>;
-  neq: NonNullValue<NonNull, string>;
-  contains: string;
-  endsWith: string;
-  startsWith: string;
-};
+type WhereOperatorString<NonNull extends true | false> =
+  | {
+      eq: NonNullValue<NonNull, string>;
+      neq: NonNullValue<NonNull, string>;
+      contains: string;
+      endsWith: string;
+      startsWith: string;
+    }
+  | NonNullValue<NonNull, string>;
 
-type WhereOperatorNumber<NonNull extends true | false> = {
-  eq: NonNullValue<NonNull, number>;
-  neq: NonNullValue<NonNull, number>;
-  gt: number;
-  lt: number;
-  gte: number;
-  lte: number;
-  between: [number, number];
-};
+type WhereOperatorNumber<NonNull extends true | false> =
+  | {
+      eq: NonNullValue<NonNull, number>;
+      neq: NonNullValue<NonNull, number>;
+      gt: number;
+      lt: number;
+      gte: number;
+      lte: number;
+      between: [number, number];
+    }
+  | NonNullValue<NonNull, number>;
 
-type WhereOperatorBoolean<NonNull extends true | false> = {
-  eq: NonNullValue<NonNull, boolean>;
-  neq: NonNullValue<NonNull, boolean>;
-};
+type WhereOperatorBoolean<NonNull extends true | false> =
+  | {
+      eq: NonNullValue<NonNull, boolean>;
+      neq: NonNullValue<NonNull, boolean>;
+    }
+  | NonNullValue<NonNull, boolean>;
 
 // I have no idea what I'm doing but this appears to work
 // T extends null will result in "boolean" instead of true for some reason (perhaps because T extends unknown | null??)
 // but false works just fine, so we check for that and "force" the "boolean" into a "true"
-type IsNullable<T> = (T extends null ? true : false) extends false
-  ? false
-  : true;
+type IsNotNullable<T> = (T extends null ? true : false) extends false
+  ? true
+  : false;
 
 export class UserModel {
   static create(data: Partial<UserData>) {}
@@ -73,11 +72,11 @@ export class UserModel {
       where: Partial<{
         [Key in keyof UserData]: Partial<
           NonNullable<UserData[Key]> extends string
-            ? WhereOperatorString<IsNullable<UserData[Key]>>
+            ? WhereOperatorString<IsNotNullable<UserData[Key]>>
             : NonNullable<UserData[Key]> extends number
-            ? WhereOperatorNumber<IsNullable<UserData[Key]>>
+            ? WhereOperatorNumber<IsNotNullable<UserData[Key]>>
             : NonNullable<UserData[Key]> extends boolean
-            ? WhereOperatorBoolean<IsNullable<UserData[Key]>>
+            ? WhereOperatorBoolean<IsNotNullable<UserData[Key]>>
             : unknown
         >;
       }>;
@@ -95,8 +94,7 @@ export class UserModel {
 
 UserModel.read({
   where: {
-    random: {
-      eq: "hello",
-    },
+    username: "hello",
+    random: null,
   },
 });
